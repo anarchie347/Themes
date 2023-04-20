1c10b21c9f3ba637d42fe858d33ff3a1c7e84494
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2023-04-20 20:40:32 +0100

    Removed default color values of white

--- Themes/Theme.cs
+++ Themes/Theme.cs
@@ -15,22 +15,22 @@ namespace Anarchie.Themes
         /// <summary>
         /// The primary <see cref="Color"/> for the <see cref="Theme"/>
         /// </summary>
-        public Color PrimaryColor { get; init; } = Color.White;
+        public Color PrimaryColor { get; init; }
 
         /// <summary>
         /// The secondary <see cref="Color"/> for the <see cref="Theme"/>
         /// </summary>
-        public Color SecondaryColor { get; init; } = Color.White;
+        public Color SecondaryColor { get; init; }
 
         /// <summary>
         /// The <see cref="Color"/> for the background of the <see cref="ThemeableForm{ThemeType}"/>
         /// </summary>
-        public Color FormBackColor { get; init; } = Color.White;
+        public Color FormBackColor { get; init; }
 
         /// <summary>
         /// The <see cref="Image"/> for the background of the <see cref="ThemeableForm{ThemeType}"/>>
         /// </summary>
-        public Image? FormBackgroundImage { get; init; } = null;
+        public Image? FormBackgroundImage { get; init; }
 
     }
 }
